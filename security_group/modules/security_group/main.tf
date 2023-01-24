d3092c9ef4236bfd8f235edcabb54df8a4a3760e
Luiz Felipe Bernardo <br.betech@gmail.com>
2023-01-23 21:40:09 -0300

    create module security group

---
+++ security_group/modules/security_group/main.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "security_group" {
+  vpc_id      = var.vpc_id
+  name        = "example-sg"
+  description = "example security group"
+
+  ingress {
+    from_port   = var.ingress_rules[0].from_port
+    to_port     = var.ingress_rules[0].to_port
+    protocol    = var.ingress_rules[0].protocol
+    cidr_blocks = var.ingress_rules[0].cidr_blocks
+  }
+
+  egress {
+    from_port   = var.egress_rules[0].from_port
+    to_port     = var.egress_rules[0].to_port
+    protocol    = var.egress_rules[0].protocol
+    cidr_blocks = var.egress_rules[0].cidr_blocks
+  }
+}
